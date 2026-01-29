ee820a52e82493ca05df75630c0cb5cc6d0afd42
Lozlink <mark.mekhayl@gmail.com>
2026-01-29 11:42:50 +1100

    lol

---
+++ app.config.ts
@@ -0,0 +1,73 @@
+export default {
+  expo: {
+    name: "Real Estate CRM",
+    slug: "realestate-crm",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    scheme: "realestate-crm",
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#6200ee"
+    },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.realestate-geo.crm",
+      infoPlist: {
+        NSContactsUsageDescription: "Allow $(PRODUCT_NAME) to access your contacts."
+      },
+      config: {
+        googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
+      }
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#6200ee"
+      },
+      edgeToEdgeEnabled: true,
+      package: "com.realestate.crm",
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_MAPS_API_KEY
+        }
+      },
+      permissions: [
+        "android.permission.ACCESS_COARSE_LOCATION",
+        "android.permission.ACCESS_FINE_LOCATION",
+        "android.permission.READ_CONTACTS"
+      ]
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+      bundler: "metro"
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-location",
+        {
+          locationAlwaysAndWhenInUsePermission: "Allow $(PRODUCT_NAME) to use your location."
+        }
+      ],
+      [
+        "expo-contacts",
+        {
+          contactsPermission: "Allow $(PRODUCT_NAME) to access your contacts."
+        }
+      ]
+    ],
+    extra: {
+      SUPABASE_URL: process.env.SUPABASE_URL,
+      SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+      GOOGLE_PLACES_API_KEY: process.env.GOOGLE_PLACES_API_KEY,
+      router: {},
+      eas: {
+        projectId: "aed13454-1087-4558-a758-266f2bdddb63"
+      }
+    }
+  }
+};
